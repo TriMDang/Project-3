181cae43a4bb5900375952ff5e1e3289e5ca6801
Andrew <164203018+andrewnguyxn@users.noreply.github.com>
2024-12-02 21:36:46 -0500

    Update Set.cpp

--- Set.cpp
+++ Set.cpp
@@ -1,87 +1,91 @@
-#include "Set.hpp"
 #include <iostream>
+#include "Set.hpp"
 #include "customexceptions.hpp"
-#include <cstddef>
+using namespace std;
 
+// ============================================================
+// Default Constructor.
+// Creates a hashSet with table_size 0
+// Parameter: None.
+// Return: None.
+// ============================================================
+template <class T>
+    Set<T>::Set(size_t table_size) : hashSet(table_size) {}
 
-
-//==============================================================
-// Default Constructor
-// Author: Tri Dang
-// Description: Initializes an empty Red-Black Tree.
-//==============================================================
-template <typename K, typename V>
-Set<K, V>::Set(size_t size) : hash_set(size) {
-}
-
-
-//==============================================================
-// Copy Constructor
-// Author: Tri Dang
-// Description: Initializes an empty Red-Black Tree.
-//==============================================================
-template <typename K, typename V>
-Set<K, V>::Set(const Set<K, V>& otherSet){
-    hash_set = otherSet.hash_set;
-}
-
-//==============================================================
-// Destructor
-// Author: Tri Dang
-// Description: Initializes an empty Red-Black Tree.
-//==============================================================
-template <typename K, typename V>
-Set<K, V>::~Set(){
-}
-
-//==============================================================
-// Operator=
-// Author: Tri Dang
-// Description: Initializes an empty Red-Black Tree.
-//==============================================================
-template <typename K, typename V>
-Set<K, V>& Set<K, V>::operator=(const Set<K, V>& otherSet){
-    hash_set = otherSet.hash_set;
-    return *this;
-}
-
-//==============================================================
-// Search
-// Author: Tri Dang
-// Description: Searches through the hash table to find the value. Return true if value do exist
-//==============================================================
-template <typename K, typename V>
-bool Set<K, V>::setSearch(const K& value) const{
-    if (hash_set.search(value) != nullptr){
-        return true;
-    } else{
-        return false;
+// ============================================================
+// Copy Constructor.
+// Creates a hashSet and deep copies the other hashSet
+// Parameter:
+//  - other: The Set object to copy from.
+// Return: None.
+// ============================================================
+template <class T>
+    Set<T>::Set(const Set<T> &other) {
+        hashSet(other.hashSet);
     }
-}
 
-//==============================================================
-// Insert
-// Author: Tri Dang
-// Description: Throw error if value existed. Otherwise, insert it into hash_set
-//==============================================================
-template <typename K, typename V>
-void Set<K, V>::insert(const K& value){
-    if (setSearch(value) == true){ // if value DOES exist
-        throw ValueExisted();
-    } else{
-        hash_set.insert(value, value); // Since a value in a set is UNIQUE, the value can also be its key 
+// ============================================================
+// Destructor.
+// Deallocate a set, deleting it
+// Parameter: None.
+// Return: None.
+// ============================================================
+template <class T>
+    Set<T>::~Set() {
     }
-}
-//==============================================================
-// Remove
-// Author: Tri Dang
-// Description: Throw error if value does not exist. Otherwise, delete it.
-//==============================================================
-template <typename K, typename V>
-void Set<K, V>::remove(const K& value){
-    if (setSearch(value) == true){ // if value does exist
-        hash_set.remove(value);
-    } else{
-        throw ValueNotInTreeException();
+
+// ============================================================
+// operator=
+// Assign hashSet to other's hashSet using hashSet.cpp operator=
+// Parameter: the other set
+// Return: the set being called
+// ============================================================
+template <class T>
+    Set<T> Set<T>::operator=(const Set<T> &other) {
+        if (this != &other) {
+            hashSet = other.hashSet;
+            // tableSize = other.tableSize;
+        }
+        return (*this);
     }
-}
+
+// ============================================================
+// Insert Method.
+// Inserts a value into the set, throw exception error if value existed
+// Parameter: value - The value to insert.
+// Return: None.
+// ============================================================
+template <class T>
+    void Set<T>::insert(const T &value) {
+        if (search(value) == true){
+            throw valueExisted();
+        } else{
+            hashSet.insert(value, value); // key can be the value since value must be unique
+        }
+    }
+
+// ============================================================
+// Remove Method.
+// Removes a value from the set, throw exception error if value is nonexistent
+// Parameter: value - The value to remove.
+// Return: None.
+// ============================================================
+template <class T>
+    void Set<T>::remove(const T& value) {
+        if (search(value) == true){
+            hashSet.remove(value);
+        } else{
+            throw ValueNotInTreeException();
+        }
+    }
+
+// ============================================================
+// Search Method.
+// Searches for a value in the set.
+// Parameter: valueHashMap
+// Return: True if value exist, oterwise false.
+// ============================================================
+template <class T>
+    bool Set<T>::search(const T& value) {
+        return hashSet.search(value) != nullptr;
+    }
