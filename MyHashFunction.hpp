2726648e18d5bfa506d8089188a3c236d4a370c5
Andrew <164203018+andrewnguyxn@users.noreply.github.com>
2024-12-02 21:35:32 -0500

    Update MyHashFunction.hpp

--- MyHashFunction.hpp
+++ MyHashFunction.hpp
@@ -1,15 +1,9 @@
-//==============================================================
-// Names: Aisha Barry, Tri Dang, Andre Nguyen
-// Class: CS 271-01
-// Date: 11/21/2024
-// About: HashMap.hpp contains the declarations for the
-// MyHashFunction
-//==============================================================
-
 #ifndef MyHashFunction_HPP
 #define MyHashFunction_HPP
+using namespace std;
 
 #include <type_traits>  // For type traits
+#include <cstddef>
 
 template <typename K>
 class MyHashFunction {
@@ -18,11 +12,11 @@ private:
     long m;  // The number of buckets in the hash table
 
     // Static assertion to ensure K is numeric
-    static_assert(std::is_arithmetic<K>::value, "Key type must be numeric");
+    static_assert(is_arithmetic<K>::value, "Key type must be numeric");
 
 public:
     // Constructor
-    MyHashFunction(long constant_k, long table_size);
+    explicit MyHashFunction(size_t table_size);
 
     // Destructor
     ~MyHashFunction();
